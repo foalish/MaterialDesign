f5454c3662d892683e963755fd4e0448d8f3e5fd
foalish <foalefamily@gmail.com>
2015-11-12 12:44:46 +0000

    first commit

---
+++ app/src/main/java/com/david/fixedtabsappbarlayout/ThreeFragment.java
@@ -0,0 +1,24 @@
+package com.david.fixedtabsappbarlayout;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public class ThreeFragment extends Fragment{
+
+    public ThreeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+ //   @Override
+ //   public View onCreateView(LayoutInflater inflater, ViewGroup container,
+   //                          Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+     //   return inflater.inflate(R.layout.fragment_three, container, false);
+//    }
+
+ }
---
+++ app/src/main/java/com/david/fixedtabsappbarlayout/TwoFragment.java
@@ -0,0 +1,24 @@
+package com.david.fixedtabsappbarlayout;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public class TwoFragment extends Fragment{
+
+    public TwoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+  // @Override
+   // public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    //                         Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+   //     return inflater.inflate(R.layout.fragment_two, container, false);
+  //  }
+
+}
